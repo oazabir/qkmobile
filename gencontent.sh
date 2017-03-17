4b1d067c945167620a71547c20aac82ab3f7750d
Omar AL Zabir <omaralzabir@gmail.com>
2017-03-17 10:34:53 +0000

    update content fetch scripts

--- gencontent.sh
+++ gencontent.sh
@@ -2,7 +2,8 @@
 rm quranerkotha.com/ -rf
 
 # Reap latest content from website
-wget -E -U mozilla -r --convert-links -nc --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex reply https://quranerkotha.com/index/
+wget -E -U mozilla -r --convert-links -nc --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex "reply|feed/|amp/|robots.txt" https://quranerkotha.com/index/
+
 
 # Delete useless folders
 find quranerkotha.com/ -type d -name 'feed' -exec rm -rf {} \;
--- updatecontent.sh
+++ updatecontent.sh
@@ -3,7 +3,7 @@ FOLDER=$1
 rm quranerkotha.com/$FOLDER -rf
 
 # Reap latest content from website
-wget -E -U mozilla --include-directories=$FOLDER,wp-content/uploads -l1 -r --convert-links -nc --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex reply https://quranerkotha.com/$FOLDER/
+wget -E -U mozilla --include-directories=$FOLDER,wp-content/uploads -l1 -r --convert-links --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex "reply|.pdf|feed|amp|fbicon.png|robots.txt" https://quranerkotha.com/$FOLDER/
 
 # Delete useless folders
 find quranerkotha.com/$FOLDER/ -type d -name 'feed' -exec rm -rf {} \;
