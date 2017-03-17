db418af2c8aea00c5b256d4570678e3c8e35584c
Omar Al Zabir <Omar Al Zabir>
2017-03-17 11:13:13 +0000

    new build of apk

--- quranerkotha.app/build.bat
+++ quranerkotha.app/build.bat
@@ -6,4 +6,4 @@ copy ..\appbuild\android\release-unsigned.apk ..\appbuild\android\release-signed
 cmd /c jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ..\appbuild\android\release-signed.apk quranerkotha
 del ..\appbuild\quranerkotha.apk
 cmd /c %ANDROID_HOME%\build-tools\25.0.2\zipalign 4 ..\appbuild\android\release-signed.apk ..\appbuild\quranerkotha.apk
-
+copy ..\appbuild\quranerkotha.apk .\
