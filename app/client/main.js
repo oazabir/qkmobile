96ef6bec125fc0bb6085c6dd9efed03b7ab7e61b
Omar AL Zabir <omaralzabir@gmail.com>
2017-03-27 22:58:56 +0100

    Gray out Visited links

--- app/client/main.js
+++ app/client/main.js
@@ -34,12 +34,41 @@ Meteor.startup(function() {
     if (lastPath) {
       if (lastPath != '/index/index.html') {
         iframe.attr('src', lastPath);
+      } else {
       }
+
+
     }
     iframe.on("load", function() {
-      var path = $('#iframe').contents()[0].location.pathname;
+      var path = iframe.contents()[0].location.pathname;
       console.log(path);
       window.localStorage.setItem("lastPath", path);
+
+      var visitedLinks = window.localStorage.getItem("visitedItems");
+      if (visitedLinks){
+        visitedLinks = eval(visitedLinks);
+      } else {
+        visitedLinks = [];
+      }
+
+      if ($.inArray(path, visitedLinks)<0){
+        visitedLinks.push(path);
+      }
+
+      window.localStorage.setItem("visitedItems", JSON.stringify(visitedLinks));
+
+      var viewedLinks = eval(window.localStorage.getItem("visitedItems") || "[]");
+      $('a', iframe.contents()[0]).each(function(){
+        var href = $(this).attr('href');
+        for (var i = 0; i < viewedLinks.length; i ++){
+          if(viewedLinks[i].length > 3){
+            if (href.indexOf(viewedLinks[i])>=0) {
+              $(this).addClass('visited');
+            } 
+          }
+        }
+      });
+
     });
   }
 
