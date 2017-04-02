cfb3e07392844d0eb8b2f2d491032a8f97170b63
Omar AL Zabir <omaralzabir@gmail.com>
2017-04-02 10:05:29 +0100

    make app work on android 4.2.2

--- app/client/main.js
+++ app/client/main.js
@@ -26,55 +26,61 @@ Meteor.startup(function() {
     // Call syncHashedEmail anywhere in your app if you have the user's email.
     // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
     // window.plugins.OneSignal.syncHashedEmail(userEmail);
+    
   }
 
   if(Meteor.isClient) {
     var iframe = $('#iframe');
-    var lastPath = window.localStorage.getItem("lastPath");
-    if (lastPath) {
-      if (lastPath != '/index/index.html') {
-        iframe.attr('src', lastPath);
-      } else {
-      }
-
-
-    }
-    iframe.on("load", function() {
-      // Remember the last article loaded so that we can reload it when app starts
-      var path = iframe.contents()[0].location.pathname;
-      console.log(path);
-      window.localStorage.setItem("lastPath", path);
-
-      // remember the visited links in an array so that we can mark the hyperlinks as visited
-      var visitedLinks = window.localStorage.getItem("visitedItems");
-      if (visitedLinks){
-        visitedLinks = eval(visitedLinks);
-      } else {
-        visitedLinks = [];
-      }
-
-      if ($.inArray(path, visitedLinks)<0){
-        visitedLinks.push(path);
-      }
-
-      if (visitedLinks.length > 100) {
-        visitedLinkes = visitedLinks.slice(0, 100);
-      }
-      window.localStorage.setItem("visitedItems", JSON.stringify(visitedLinks));
-
-      var viewedLinks = eval(window.localStorage.getItem("visitedItems") || "[]");
-      $('a', iframe.contents()[0]).each(function(){
-        var href = $(this).attr('href');
-        for (var i = 0; i < viewedLinks.length; i ++){
-          if(viewedLinks[i].length > 3){
-            if (href.indexOf(viewedLinks[i])>=0) {
-              $(this).addClass('visited');
-            } 
-          }
+    window.setInterval(function(){
+      iframe.css('width',window.innerWidth+'px');
+      iframe.css('height',window.innerHeight+'px');
+    }, 1000);
+    if (window.localStorage) {
+      var lastPath = window.localStorage.getItem("lastPath");
+      if (lastPath) {
+        if (lastPath != '/index/index.html') {
+          iframe.attr('src', lastPath);
+        } else {
         }
-      });
+      }
 
-    });
+      iframe.on("load", function() {
+        // Remember the last article loaded so that we can reload it when app starts
+        var path = iframe.contents()[0].location.pathname;
+        console.log(path);
+        window.localStorage.setItem("lastPath", path);
+
+        // remember the visited links in an array so that we can mark the hyperlinks as visited
+        var visitedLinks = window.localStorage.getItem("visitedItems");
+        if (visitedLinks){
+          visitedLinks = eval(visitedLinks);
+        } else {
+          visitedLinks = [];
+        }
+
+        if ($.inArray(path, visitedLinks)<0){
+          visitedLinks.push(path);
+        }
+
+        if (visitedLinks.length > 100) {
+          visitedLinkes = visitedLinks.slice(0, 100);
+        }
+        window.localStorage.setItem("visitedItems", JSON.stringify(visitedLinks));
+
+        var viewedLinks = eval(window.localStorage.getItem("visitedItems") || "[]");
+        $('a', iframe.contents()[0]).each(function(){
+          var href = $(this).attr('href');
+          for (var i = 0; i < viewedLinks.length; i ++){
+            if(viewedLinks[i].length > 3){
+              if (href.indexOf(viewedLinks[i])>=0) {
+                $(this).addClass('visited');
+              } 
+            }
+          }
+        });
+
+      });
+    }
   }
 
 });
