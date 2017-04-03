49288473c7a16f84fab7a90816c77b9fed806f2c
Omar AL Zabir <omaralzabir@gmail.com>
2017-04-03 12:29:49 +0100

    Baqarah 274

--- addarticle.sh
+++ addarticle.sh
@@ -2,8 +2,9 @@
 ARTICLE=$1
 ./updatecontent.sh $ARTICLE
 ./updatecontent.sh index
-git add $ARTICLE
-git add index/index.html
+git add quranerkotha.com/$ARTICLE
+git add quranerkotha.com/index/index.html
+git add quranerkotha.com/wp-content/uploads
 git commit -m "$ARTICLE added"
 git status
 
