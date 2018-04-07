f7bff8c10896b1712f6b970e00ed83726fdc0682
Omar AL Zabir <omaralzabir@gmail.com>
2018-04-07 17:13:34 +0800

    Setup logger on server

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Logger }        from 'meteor/ostrio:logger';
+import { LoggerConsole } from 'meteor/ostrio:loggerconsole';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  const mylog = new Logger();
+  (new LoggerConsole(mylog)).enable();
+
+  // Initialize and enable LoggerConsole with custom formatting:
+  (new LoggerConsole(log, {
+    format(opts) {
+      return ((Meteor.isServer) ? '[SERVER]' : "[CLIENT]") + ' [' + opts.level + '] - ' + opts.message;
+    }
+  })).enable();
+
+  log.info("Started logger");
 });
