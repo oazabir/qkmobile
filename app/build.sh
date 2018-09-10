84def8021c516e66c5403d855b2ca071ce212265
Omar Al Zabir <omaralzabir@gmail.com>
2018-09-10 02:55:53 +0000

    New app built

--- app/build.sh
+++ app/build.sh
@@ -5,7 +5,7 @@ sed -i.bak  "s/$version/$newversion/" mobile-config.js
 
 #export ANDROID_HOME=~/Library/Android/sdk
 meteor build ../appbuild --server=https://app.quranerkotha.com --architecture=os.linux.x86_64 && \
-\ # Copy the apk out to appbuild folder and sign, compress it
+\
 cp ../appbuild/android/project/build/outputs/apk/release/android-release-unsigned.apk ../appbuild/android/release-signed.apk && \
 \
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ../appbuild/android/release-signed.apk quranerkotha && \
