39406f27608e27b4bc8f0802f8ef54e011749e5a
Omar AL Zabir <omaralzabir@gmail.com>
2019-08-15 13:56:02 +0100

    change updatecontent to skip images

--- updatecontent.sh
+++ updatecontent.sh
@@ -1,24 +1,24 @@
 set -x
 FOLDER=$1
 # Drop existing folder
-rm quranerkotha.com/$FOLDER -rf
+rm -rf quranerkotha.com/$FOLDER
 
 # Reap latest content from website
-wget -E -U mozilla --include-directories=$FOLDER,wp-content/uploads -l1 -r --convert-links --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex "reply|.pdf|feed|amp|fbicon.png|robots.txt" https://quranerkotha.com/$FOLDER/
-
+#wget -E -U mozilla --include-directories=$FOLDER,wp-content/uploads -l1 -r --convert-links --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --exclude-directories=tag,category,amp,feed --reject-regex "reply|.pdf|feed|amp|fbicon.png|robots.txt" https://quranerkotha.com/$FOLDER/
+wget -E -U mozilla -r -nc --user-agent "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --domains=quranerkotha.com --ignore-tags=link,script --exclude-directories=tag,category,amp,feed,page,author,pdf,wp-includes,wp-json --accept-regex ".*/$|(/)(/.*/)index.html$" https://quranerkotha.com/$FOLDER/
 # Delete all diff resolution files
-rm quranerkotha.com/wp-content/uploads/*/*/*-*x*.png
-rm quranerkotha.com/wp-content/uploads/*/*/*-*x*.jpg
+#rm quranerkotha.com/wp-content/uploads/*/*/*-*x*.png
+#rm quranerkotha.com/wp-content/uploads/*/*/*-*x*.jpg
 
 # Delete useless folders
-find quranerkotha.com/$FOLDER/ -type d -name 'feed' -exec rm -rf {} \;
-find quranerkotha.com/$FOLDER/ -type d -name 'amp' -exec rm -rf {} \;
+#find quranerkotha.com/$FOLDER/ -type d -name 'feed' -exec rm -rf {} \;
+#find quranerkotha.com/$FOLDER/ -type d -name 'amp' -exec rm -rf {} \;
 
 sedi () {
     sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
 }
-sedi  's/-150x150.png/.png/g' 'quranerkotha.com/index/index.html'
-sedi  's/-150x150.jpg/.jpg/g' 'quranerkotha.com/index/index.html'
+#sedi  's/-150x150.png/.png/g' 'quranerkotha.com/index/index.html'
+#sedi  's/-150x150.jpg/.jpg/g' 'quranerkotha.com/index/index.html'
 
 ./cleanupcontent.sh ./quranerkotha.com/$FOLDER/
 
