bc60c281099ef73cbbc4fc234180334a865e2d54
omaralzabir <omaralzabir@gmail.com>
2019-08-15 01:38:26 -0400

    Update server build

--- docker/build-server.sh
+++ docker/build-server.sh
@@ -3,13 +3,23 @@
 set -e
 
 : ${APP_PATH:?}
+: ${PUBLIC:?}
 : ${APP_BUILD_PATH:?}
 : ${APP_SERVER:?}
+: ${PORT:?}
 
 mkdir -p /tmp/app
 mkdir -p /tmp/appbuild
 
-cp -r ${APP_PATH}/. /tmp/app/
+cd /tmp/app
+cp -rd ${APP_PATH}/* ./
+rm -rf ./public || echo "Cannot delete public folder"
+mkdir ./public
+cp -r ${PUBLIC}/* ./public/ || echo "Public content copy failed."
+
+echo "This is how app directory looks like..."
+ls 
+ls ./public/
 
 cd /tmp/app
 bash meteor_setup.sh
@@ -20,4 +30,4 @@ cd /tmp/appbuild/bundle/programs/server
 npm install --production
 
 cd /tmp/appbuild/bundle
-MONGO_URL=mongodb://localhost:27017/quranerkotha ROOT_URL=${APP_SERVER} node main.js
+MONGO_URL=mongodb://localhost:27017/quranerkotha ROOT_URL=${APP_SERVER} PORT=${PORT} node main.js
--- docker/run_server.sh
+++ docker/run_server.sh
@@ -1,10 +1,12 @@
 export APP_SERVER=https://app.quranerkotha.com
+export PORT=3000
 docker build -f Server.Dockerfile -t qkmobile-server . && \
 docker run \
     -d \
     --rm \
     --name qkmobile-server \
     -e APP_SERVER="$APP_SERVER" \
+    -e PORT="$PORT" \
     -v "$PWD/../app":/app \
     -v "$PWD/../appbuild":/build \
     -v "$PWD/../quranerkotha.com":/app/public \
